b55450f96b2f900bdf4c482dc09e65d620cb5b33
Cong Shen <shenc0411@gmail.com>
2017-10-13 14:17:50 -0500

    still working on it

--- mp3/poissonBlend.m
+++ mp3/poissonBlend.m
@@ -1,5 +1,20 @@
 function [im_out] = poissonBlend(im_source, im_mask, im_background)
-    im_foregorund = im_mask .* im_source;
-    
-    im_out = im_background;
+    [height, width, num_channels] = size(im_background);
+    im_out = ~im_mask .* im_background;
+    for channel = 1 : num_channels
+        e = 0;
+        A = sparse(height * width, height * width);
+        b = zeros(height * width, 1);
+        im2var = zeros(height, width);
+        im2var(1 : height * width) = 1 : height * width;
+        for x = 1 : width
+            for y = 1 : height
+                if im_mask(y, x, channel) == 0
+                    e = e + 1;
+                end
+            end
+        end
+        A = A(1 : e, :);
+        b = b(1 : e);
+    end
 end

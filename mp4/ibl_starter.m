fa79242d12f1ef98e14673504dcf8154b51ff6e6
Cong Shen <shenc0411@gmail.com>
2017-10-23 21:39:41 -0500

    eliminated some hardcoding stuff

--- mp4/ibl_starter.m
+++ mp4/ibl_starter.m
@@ -1,27 +1,33 @@
 %//
-ldr1 = im2double(imread('src_img/dark.jpg'));
-ldr2 = im2double(imread('src_img/normal.jpg'));
-ldr3 = im2double(imread('src_img/bright.jpg'));
+ldr_folder_path = 'src_img\ldr_img\';
+ldr_size = 512;
+ldr_files = dir(ldr_folder_path);
+[length, d] = size(ldr_files);
+count = 0;
+for i = 1 : length
+    file_name = ldr_files(i).name;
+    if file_name(1) == "."
+        continue;
+    end
+    file_path = [ldr_folder_path '' file_name];
+    count = count + 1;
+    
+    ldr = im2double(imread(file_path));
+    ldr = imresize(ldr, [ldr_size, ldr_size], 'bilinear');
+    ldrs(:, :, :, count) = ldr;
+    
+    info = imfinfo(file_path);
+    info_dc = info.DigitalCamera;
+    exposures(count) = info_dc.ExposureTime;
+end
 
-ldr1 = imresize(ldr1, [512, 512], 'bilinear');
-ldr2 = imresize(ldr2, [512, 512], 'bilinear');
-ldr3 = imresize(ldr3, [512, 512], 'bilinear');
 
-%Naive LDR merging
-info_ldr1 = imfinfo('src_img/dark.jpg');
-info_ldr1_dc = info_ldr1.DigitalCamera;
-info_ldr2 = imfinfo('src_img/normal.jpg');
-info_ldr2_dc = info_ldr2.DigitalCamera;
-info_ldr3 = imfinfo('src_img/bright.jpg');
-info_ldr3_dc = info_ldr3.DigitalCamera;
+hdr_naive = makehdr_naive(ldrs, exposures);
+hdr_naive_tonemapped = tonemap(hdr_naive);
+figure(666), imshow(hdr_naive_tonemapped), title('Naive HDR');
 
-ldrs = cat(4, ldr1, ldr2, ldr3);
-exp = [info_ldr1_dc.ExposureTime, info_ldr2_dc.ExposureTime, info_ldr3_dc.ExposureTime];
+hdr_naive_weighted = makehdr_naive_weighted(ldrs, exposures);
+hdr_naive_weighted_tonemapped = tonemap(hdr_naive_weighted);
+figure(667), imshow(hdr_naive_weighted_tonemapped), title('Weighted Naive HDR');
 
-hdr_naive = makehdr_naive(ldrs, exp);
-figure(666), imshow(tonemap(hdr_naive));
-
-hdr_naive_wuo = makehdr_naive_wuo(ldrs, exp);
-figure(667), imshow(tonemap(hdr_naive_wuo));
-
-sum(hdr_naive_wuo(:) - hdr_naive(:))
+% disp(sqrt(sum((hdr_naive_wuo_tonemapped(:) - hdr_naive_tonemapped(:)) .^ 2)));
--- mp4/makehdr_naive_weighted.m
+++ mp4/makehdr_naive_weighted.m
@@ -1,4 +1,4 @@
-function hdr = makehdr_naive_wuo(ldrs, exposures)
+function hdr = makehdr_naive_weighted(ldrs, exposures)
     % ldrs is an m x n x 3 x k matrix which can be created with ldrs = cat(4, ldr1, ldr2, ...);
     % exposures is a vector of exposure times (in seconds) corresponding to ldrs
     [exposures,sortexp] = sort(reshape(exposures,1,1,1,[]));
@@ -7,11 +7,12 @@ function hdr = makehdr_naive_wuo(ldrs, exposures)
     hdr = zeros(height, width, channels);
     
     w = @(z) double(128-abs(z-128));
-    
+    total_weight = 0;
     for i = 1 : length
         weight = w(ldrs(:, :, :, i));
+        total_weight = total_weight + weight;
         hdr = hdr + weight .* ldrs(:, :, :, i) ./ exposures(i);
     end
     
-    hdr = hdr ./ length;
+    hdr = hdr ./ total_weight;
 end

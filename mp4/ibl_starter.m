4931a4303e5a5bf4c43023707b7e74b4e29c93f4
Cong Shen <shenc0411@gmail.com>
2017-10-23 21:43:57 -0500

    some small changes

--- mp4/ibl_starter.m
+++ mp4/ibl_starter.m
@@ -1,9 +1,11 @@
-%//
+
 ldr_folder_path = 'src_img\ldr_img\';
 ldr_size = 512;
+
 ldr_files = dir(ldr_folder_path);
 [length, d] = size(ldr_files);
 count = 0;
+
 for i = 1 : length
     file_name = ldr_files(i).name;
     if file_name(1) == "."
@@ -25,9 +27,9 @@ end
 hdr_naive = makehdr_naive(ldrs, exposures);
 hdr_naive_tonemapped = tonemap(hdr_naive);
 figure(666), imshow(hdr_naive_tonemapped), title('Naive HDR');
+hdrwrite(hdr_naive, "naive_hdr.hdr");
 
 hdr_naive_weighted = makehdr_naive_weighted(ldrs, exposures);
 hdr_naive_weighted_tonemapped = tonemap(hdr_naive_weighted);
 figure(667), imshow(hdr_naive_weighted_tonemapped), title('Weighted Naive HDR');
-
-% disp(sqrt(sum((hdr_naive_wuo_tonemapped(:) - hdr_naive_tonemapped(:)) .^ 2)));
+hdrwrite(hdr_naive_weighted, "weighted_naive_hdr.hdr");

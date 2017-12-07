8d0fed830e13862b14b4b47387b651c19753be03
Shenc0411 <shenc0411@gmail.com>
2017-12-06 18:01:12 -0600

    fixed orientation

--- project/source/draw_stroke.m
+++ project/source/draw_stroke.m
@@ -1,21 +1,22 @@
-function im_out = draw_stroke(im, cx, cy, sigma, length, theta)
+function im_out = draw_stroke(im, color, cx, cy, sigma, length, theta)
 
 %DRAW_STROKE Draw a stroke on input image
 %   Input:  im - input image
 %           im_mask - mask pixels that has already been sampled
 %           cx, cy - stroke center
+%           color - stroke color
 %           radius - stroke radius
 %           length - stroke length
 %           theta - stroke orientation
 im_out = im;
 
 mask = lineMask(size(im), [cy + sin(theta) * length / 2 cx - cos(theta) * length /2], [cy - sin(theta) * length / 2 cx + cos(theta) * length /2], sigma);
-mask(mask < 0.05) = 0;
+mask(mask < 0.01) = 0;
 mask(mask ~= 0) = 1;
-% figure(2), imshow(mask);
-stroke(:, :, 1) = mask .* im(cy, cx, 1);
-stroke(:, :, 2) = mask .* im(cy, cx, 2);
-stroke(:, :, 3) = mask .* im(cy, cx, 3);
+
+stroke(:, :, 1) = mask .* color(1);
+stroke(:, :, 2) = mask .* color(2);
+stroke(:, :, 3) = mask .* color(3);
 
 im_out(stroke ~= 0) = stroke(stroke ~= 0);
 
--- project/source/fixed_orientation.m
+++ project/source/fixed_orientation.m
@@ -2,23 +2,29 @@ close all;
 clear;
 
 theta = pi / 4;
-l = 40;
-sigma = 3;
+l = 10;
+sigma = 0.5;
 
-im = im2double(imread('images/altgeld.jpg'));
+im = im2double(imread('images/altgeld_small.jpg'));
 sample_mask = zeros(size(im));
 sample_mask = sample_mask(:, :, 1);
         
 [height, width, ~] = size(im);
+im_out = zeros(size(im));
 
-[Y, X] = meshgrid(1 : 30 : height, 1 : 30 : width);
+[Y, X] = meshgrid(1 : 2 : height, 1 : 2 : width);
 X = X(:);
 Y = Y(:);
 to_render = randperm(length(X));
 
-for i = 1 : length(to_render)
-    im = draw_stroke(im, X(to_render(i)), Y(to_render(i)), sigma, l, theta);
-%     figure(1), imshow(im);
+for i = 1 : length(to_render) / 2
+    disp(i / length(to_render) * 100);
+    
+    cx = X(to_render(i));
+    cy = Y(to_render(i));
+    color = im(cy, cx, :);
+    im_out = draw_stroke(im_out, color, cx, cy, sigma, l, theta);
 end
 
-figure(3), imshow(im);
+figure(3), imshow(im_out);
+imwrite(im_out, 'fixed_orientation.jpg');
